96289a09b8bd0013cb6f933f7f1078d626dd6132
mri-dev <pocokxtrame@gmail.com>
2018-10-15 07:11:07 +0200

    Save

--- wp-content/themes/Avada-Child-Theme/templates/admin/posttype/termekek.php
+++ wp-content/themes/Avada-Child-Theme/templates/admin/posttype/termekek.php
@@ -1,3 +1,8 @@
+<h3>Cikkszám</h3>
+<?php $metakey = METAKEY_PREFIX . 'cikkszam'; ?>
+<?php $value = get_post_meta($post->ID, $metakey, true); ?>
+<input type="text" name="<?=$metakey?>" value="<?=$value?>">
+
 <h3>Összetevők</h3>
 <?php $metakey = METAKEY_PREFIX . 'leiras_osszetevok'; ?>
 <?php $value = get_post_meta($post->ID, $metakey, true); ?>
